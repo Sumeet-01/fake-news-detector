c2f03fb6e435ae1ea8da579504e2ac4821147e18
Sumeet Sangwan <sumeetsangwan0@gmail.com>
2025-06-17 13:48:26 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -22,4 +22,5 @@ def predict():
         return render_template('result.html', prediction=label, news=news_text)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
